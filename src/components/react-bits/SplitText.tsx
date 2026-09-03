a228ca24d9bdf4af4c8f9900a29bfe6d7a7f6d0f
YUG-2005 <yug@example.com>
2026-09-03 23:09:00 +0530

    Fix SplitText component cutting off characters with descenders

--- src/components/react-bits/SplitText.tsx
+++ src/components/react-bits/SplitText.tsx
@@ -21,9 +21,9 @@ export function SplitText({ text, className = '', delay = 0 }: SplitTextProps) {
   }, [controls, isInView]);
 
   return (
-    <div ref={ref} className={`inline-block overflow-hidden ${className}`}>
+    <div ref={ref} className={`inline-block overflow-hidden pb-2 -mb-2 ${className}`}>
       {text.split(' ').map((word, wordIndex) => (
-        <span key={wordIndex} className="inline-block whitespace-nowrap overflow-hidden">
+        <span key={wordIndex} className="inline-block whitespace-nowrap overflow-hidden pb-2 -mb-2">
           {word.split('').map((char, charIndex) => {
             return (
               <motion.span
